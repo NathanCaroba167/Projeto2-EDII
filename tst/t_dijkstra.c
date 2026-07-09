ae42d2490cf5afd32ddd3d9574f1e041d09d1ba7
Nathan Caroba <nathancaroba01@gmail.com>
2026-07-09 00:59:24 -0300

    fix(dijkstra): agora teste tem teste de sistema de peso dos tipos de caminho

--- tst/t_dijkstra.c
+++ tst/t_dijkstra.c
@@ -15,13 +15,13 @@ void setUp(void){}
 void tearDown(void) {}
 
 static Grafo montarGrafoLinear(void) {
-    Grafo g = criarGrafo();
+    Grafo g = criarGrafo(3);
 
-    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v2", 10.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v3", 30.0, 0.0));
-    inserirArestaGrafo(g, criarAresta("v1", "v2", "CEP107", "CEP108",10.0, 40.0,"Rua Santos"));
-    inserirArestaGrafo(g, criarAresta("v2", "v3", "CEP109", "CEP110",20.0, 50.0,"Rua Silva"));
+    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0, 0));
+    inserirVerticeGrafo(g, criarVertice("v2", 10.0, 0.0, 1));
+    inserirVerticeGrafo(g, criarVertice("v3", 30.0, 0.0, 2));
+    inserirArestaGrafo(g, "v1", "v2", "CEP107", "CEP108",10.0, 5.0,"Rua Santos");
+    inserirArestaGrafo(g, "v2", "v3", "CEP109", "CEP110",20.0, 10.0,"Rua Silva");
 
     return g;
 }
@@ -29,7 +29,7 @@ static Grafo montarGrafoLinear(void) {
 void teste_dijkstra_caminhoUnico_deveEncontrarUnicoCaminhoCorretamente(void) {
     Grafo g = montarGrafoLinear();
 
-    ResultadoDijkstra r = executarDijkstra(g, "v1", "v3");
+    ResultadoDijkstra r = executarDijkstra(g, "v1", "v3", pesoCurtoDijkstra);
 
     TEST_ASSERT_TRUE(encontrouCaminhoDijkstra(r));
 
@@ -37,11 +37,32 @@ void teste_dijkstra_caminhoUnico_deveEncontrarUnicoCaminhoCorretamente(void) {
     liberarGrafo(g);
 }
 
+void teste_dijkstra_caminhoUnico_custoCurtoCorreto(void) {
+    Grafo g = montarGrafoLinear();
+
+    ResultadoDijkstra r = executarDijkstra(g, "v1", "v3", pesoCurtoDijkstra);
+
+    TEST_ASSERT_EQUAL_DOUBLE(30.0, getCustoDijkstra(r));
+
+    liberarResultadoDijkstra(r);
+    liberarGrafo(g);
+}
+
+void teste_dijkstra_caminhoUnico_custoRapidoCorreto(void) {
+    Grafo g = montarGrafoLinear();
+
+    ResultadoDijkstra r = executarDijkstra(g, "v1", "v3", pesoRapidoDijkstra);
+
+    TEST_ASSERT_EQUAL_DOUBLE(4.0, getCustoDijkstra(r));
+
+    liberarResultadoDijkstra(r);
+    liberarGrafo(g);
+}
 
 void teste_dijkstra_caminhoUnico_deveListarArestasDoCaminhoCorretamente(void) {
     Grafo g = montarGrafoLinear();
 
-    ResultadoDijkstra r = executarDijkstra(g, "v1", "v3");
+    ResultadoDijkstra r = executarDijkstra(g, "v1", "v3", pesoCurtoDijkstra);
     Lista caminho = getCaminhoDijkstra(r);
 
     TEST_ASSERT_EQUAL_INT(2, getTamanhoLista(caminho));
@@ -51,20 +72,20 @@ void teste_dijkstra_caminhoUnico_deveListarArestasDoCaminhoCorretamente(void) {
     no = getProximoNoLista(no);
     Aresta a2 = getItemNoLista(no);
 
-    TEST_ASSERT_EQUAL_STRING("v2", getVerticeV2Aresta(a1));
-    TEST_ASSERT_EQUAL_STRING("v3", getVerticeV2Aresta(a2));
+    TEST_ASSERT_EQUAL_STRING("v2", getIDVerticeDestinoAresta(a1));
+    TEST_ASSERT_EQUAL_STRING("v3", getIDVerticeDestinoAresta(a2));
 
     liberarResultadoDijkstra(r);
     liberarGrafo(g);
 }
 
 static Grafo montarGrafoInacessivel(void) {
-    Grafo g = criarGrafo();
+    Grafo g = criarGrafo(3);
 
-    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v2", 1.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v3", 5.0, 0.0));
-    inserirArestaGrafo(g, criarAresta("v1", "v2", "CEP107", "CEP108",10.0, 60.0,"Rua Cuiaba"));
+    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0, 0));
+    inserirVerticeGrafo(g, criarVertice("v2", 1.0, 0.0, 1));
+    inserirVerticeGrafo(g, criarVertice("v3", 5.0, 0.0, 2));
+    inserirArestaGrafo(g, "v1", "v2", "CEP107", "CEP108",10.0, 60.0,"Rua Cuiaba");
 
     return g;
 }
@@ -72,7 +93,7 @@ static Grafo montarGrafoInacessivel(void) {
 void teste_dijkstra_destinoInacessivel_deveRetornarNaoEncontrou(void) {
     Grafo g = montarGrafoInacessivel();
 
-    ResultadoDijkstra r = executarDijkstra(g, "v1", "v3");
+    ResultadoDijkstra r = executarDijkstra(g, "v1", "v3", pesoCurtoDijkstra);
 
     TEST_ASSERT_FALSE(encontrouCaminhoDijkstra(r));
 
@@ -81,14 +102,14 @@ void teste_dijkstra_destinoInacessivel_deveRetornarNaoEncontrou(void) {
 }
 
 static Grafo montarGrafoMultiplosCaminhos(void) {
-    Grafo g = criarGrafo();
+    Grafo g = criarGrafo(3);
 
-    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v2", 0.0, 10.0));
-    inserirVerticeGrafo(g, criarVertice("v3", 10.0, 10.0));
-    inserirArestaGrafo(g, criarAresta("v1", "v2", "CEP110", "CEP111",5.0, 60.0,"Rua A"));
-    inserirArestaGrafo(g, criarAresta("v2", "v3", "CEP110", "CEP112",5.0, 60.0,"Rua B"));
-    inserirArestaGrafo(g, criarAresta("v1", "v3", "CEP113", "CEP110",15.0, 60.0,"Rua C"));
+    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0, 0));
+    inserirVerticeGrafo(g, criarVertice("v2", 0.0, 10.0, 1));
+    inserirVerticeGrafo(g, criarVertice("v3", 10.0, 10.0, 2));
+    inserirArestaGrafo(g, "v1", "v2", "CEP110", "CEP111",5.0, 60.0,"Rua A");
+    inserirArestaGrafo(g, "v2", "v3", "CEP110", "CEP112",5.0, 60.0,"Rua B");
+    inserirArestaGrafo(g, "v1", "v3", "CEP113", "CEP110",15.0, 60.0,"Rua C");
 
     return g;
 }
@@ -96,10 +117,10 @@ static Grafo montarGrafoMultiplosCaminhos(void) {
 void teste_dijkstra_multiplosCaminhos_deveEscolherMenorCaminho(void) {
     Grafo g = montarGrafoMultiplosCaminhos();
 
-    ResultadoDijkstra r = executarDijkstra(g, "v1", "v3");
+    ResultadoDijkstra r = executarDijkstra(g, "v1", "v3", pesoCurtoDijkstra);
 
     TEST_ASSERT_TRUE(encontrouCaminhoDijkstra(r));
-
+    TEST_ASSERT_EQUAL_DOUBLE(10.0, getCustoDijkstra(r));
     Lista caminho = getCaminhoDijkstra(r);
     TEST_ASSERT_EQUAL_INT(2, getTamanhoLista(caminho));
 
@@ -115,6 +136,8 @@ int main() {
 
     RUN_TEST(teste_dijkstra_caminhoUnico_deveEncontrarUnicoCaminhoCorretamente);
     RUN_TEST(teste_dijkstra_caminhoUnico_deveListarArestasDoCaminhoCorretamente);
+    RUN_TEST(teste_dijkstra_caminhoUnico_custoCurtoCorreto);
+    RUN_TEST(teste_dijkstra_caminhoUnico_custoRapidoCorreto);
     RUN_TEST(teste_dijkstra_destinoInacessivel_deveRetornarNaoEncontrou);
     RUN_TEST(teste_dijkstra_multiplosCaminhos_deveEscolherMenorCaminho);
 
