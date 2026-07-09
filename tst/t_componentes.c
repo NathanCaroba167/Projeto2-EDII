37bd783fd490533760ff9d882b4f08916a0af82f
Nathan Caroba <nathancaroba01@gmail.com>
2026-07-09 01:03:00 -0300

    fix(componentes): ajuste de alguns testes agora verificacao do total de vertices em componentes e verificacao de qual componente tem vertice indicado, funcionam corretamente

--- tst/t_componentes.c
+++ tst/t_componentes.c
@@ -15,9 +15,29 @@
 void setUp(void){}
 void tearDown(void) {}
 
+static int totalVerticesEmComponentes(ResultadoComponentes r) {
+    int total = 0;
+    for (int i = 0; i < getNumComponentes(r); i++) {
+        total += getTamanhoLista(getVerticesComponentes(r, i));
+    }
+    return total;
+}
+
+static int componenteDoVertice(ResultadoComponentes r, const char* id) {
+    for (int i = 0; i < getNumComponentes(r); i++) {
+        Lista comp = getVerticesComponentes(r, i);
+        Nopont noLista = getPrimeiroNoLista(comp);
+        while (noLista != NULL) {
+            Vertice v = (Vertice) getItemNoLista(noLista);
+            if (strcmp(getIDVertice(v), id) == 0) return i;
+            noLista = getProximoNoLista(noLista);
+        }
+    }
+    return -1;
+}
 
 void teste_executarComponentes_grafoVazio_deveRetornarZeroComponentes(void) {
-    Grafo g = criarGrafo();
+    Grafo g = criarGrafo(0);
 
     ResultadoComponentes r = executarComponentes(g, 0.0);
 
@@ -28,8 +48,8 @@ void teste_executarComponentes_grafoVazio_deveRetornarZeroComponentes(void) {
 }
 
 void teste_executarComponentes_umVerticeApenas_deveRetornarUmComponente(void) {
-    Grafo g = criarGrafo();
-    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0));
+    Grafo g = criarGrafo(1);
+    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0, 0));
 
     ResultadoComponentes r = executarComponentes(g, 0.0);
 
@@ -41,43 +61,51 @@ void teste_executarComponentes_umVerticeApenas_deveRetornarUmComponente(void) {
 }
 
 void teste_executarComponentes_grafoConexo_deveRetornarUmComponente(void) {
-    Grafo g = criarGrafo();
-    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v2", 5.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v3", 10.0, 0.0));
-    inserirArestaGrafo(g, criarAresta("v1", "v2", "CEP405", "CEP412", 40.0, 10.0, "Rua Santa Monica"));
-    inserirArestaGrafo(g, criarAresta("v2", "v3", "CEP444", "CEP417", 40.0, 10.0, "Rua Santa Angela"));
+    Grafo g = criarGrafo(3);
+    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0, 0));
+    inserirVerticeGrafo(g, criarVertice("v2", 5.0, 0.0, 1));
+    inserirVerticeGrafo(g, criarVertice("v3", 10.0, 0.0, 2));
+    inserirArestaGrafo(g, "v1", "v2", "CEP405", "CEP412", 40.0, 10.0, "Rua Santa Monica");
+    inserirArestaGrafo(g, "v2", "v3", "CEP444", "CEP417", 40.0, 10.0, "Rua Santa Angela");
 
     ResultadoComponentes r = executarComponentes(g, 5.0);
 
     TEST_ASSERT_EQUAL_INT(1, getNumComponentes(r));
-    TEST_ASSERT_EQUAL_INT(3, getVerticesComponentes(r,0));
+    TEST_ASSERT_EQUAL_INT(3, totalVerticesEmComponentes(r));
 
     liberarResultadoComponentes(r);
     liberarGrafo(g);
 }
 
 void teste_executarComponentes_verticeIsolado_deveGerarDoisComponentes(void) {
-    Grafo g = criarGrafo();
-    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v2", 5.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v3", 10.0, 0.0));
-    inserirArestaGrafo(g, criarAresta("v1", "v2", "CEP409", "CEP418", 50.0, 10.0, "Rua Julia"));
+    Grafo g = criarGrafo(3);
+    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0, 0));
+    inserirVerticeGrafo(g, criarVertice("v2", 5.0, 0.0, 1));
+    inserirVerticeGrafo(g, criarVertice("v3", 10.0, 0.0, 2));
+    inserirArestaGrafo(g, "v1", "v2", "CEP409", "CEP418", 50.0, 10.0, "Rua Julia");
 
     ResultadoComponentes r = executarComponentes(g, 5.0);
 
     TEST_ASSERT_EQUAL_INT(2, getNumComponentes(r));
-    TEST_ASSERT_EQUAL_INT(3, getTamanhoLista(getVerticesComponentes(r,0)));
+    TEST_ASSERT_EQUAL_INT(3, totalVerticesEmComponentes(r));
+
+    int comp1 = componenteDoVertice(r, "v1");
+    int comp2 = componenteDoVertice(r, "v2");
+    int comp3 = componenteDoVertice(r, "v3");
+
+    TEST_ASSERT_NOT_EQUAL(-1, comp1);
+    TEST_ASSERT_EQUAL_INT(comp1, comp2);
+    TEST_ASSERT_NOT_EQUAL(comp1, comp3);
 
     liberarResultadoComponentes(r);
     liberarGrafo(g);
 }
 
 void teste_executarComponentes_verificaVL_deveSepararQuandoVMInsuficiente(void) {
-    Grafo g = criarGrafo();
-    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v2", 5.0, 0.0));
-    inserirArestaGrafo(g, criarAresta("v1", "v2", "CEP286", "CEP589", 60.0, 5.0, "Rua Rebeca"));
+    Grafo g = criarGrafo(2);
+    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0, 0));
+    inserirVerticeGrafo(g, criarVertice("v2", 5.0, 0.0, 1));
+    inserirArestaGrafo(g, "v1", "v2", "CEP286", "CEP589", 60.0, 5.0, "Rua Rebeca");
 
     ResultadoComponentes r1 = executarComponentes(g, 5.0);
     ResultadoComponentes r2 = executarComponentes(g, 6.0);
@@ -91,41 +119,57 @@ void teste_executarComponentes_verificaVL_deveSepararQuandoVMInsuficiente(void) 
 }
 
 void teste_executarComponentes_grafoMaoUnica_deveSerFracamenteConexo(void) {
-    Grafo g = criarGrafo();
-    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v2", 5.0, 0.0));
-    inserirArestaGrafo(g, criarAresta("v1", "v2", "CEP742", "CEP157", 80.0, 10.0, "Rua Giovanna"));
+    Grafo g = criarGrafo(2);
+    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0, 0));
+    inserirVerticeGrafo(g, criarVertice("v2", 5.0, 0.0, 1));
+    inserirArestaGrafo(g, "v1", "v2", "CEP742", "CEP157", 80.0, 10.0, "Rua Giovanna");
 
     ResultadoComponentes r = executarComponentes(g, 5.0);
 
     TEST_ASSERT_EQUAL_INT(1, getNumComponentes(r));
+
+    int comp1 = componenteDoVertice(r, "v1");
+    int comp2 = componenteDoVertice(r, "v2");
+    TEST_ASSERT_EQUAL_INT(comp1, comp2);
 
     liberarResultadoComponentes(r);
     liberarGrafo(g);
 }
 
 void teste_executarComponentes_tresGruposSeparados_deveGerarTresComponentes(void) {
-    Grafo g = criarGrafo();
-    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v2", 5.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v3", 10.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v4", 15.0, 0.0));
-    inserirVerticeGrafo(g, criarVertice("v5", 20.0, 0.0));
-    inserirArestaGrafo(g, criarAresta("v1", "v2", "CEP390", "CEP391", 30.0, 10.0, "Rua Isabelle"));
-    inserirArestaGrafo(g, criarAresta("v3", "v4", "CEP419", "CEP420", 30.0, 10.0, "Rua Ana"));
+    Grafo g = criarGrafo(5);
+    inserirVerticeGrafo(g, criarVertice("v1", 0.0, 0.0, 0));
+    inserirVerticeGrafo(g, criarVertice("v2", 5.0, 0.0, 1));
+    inserirVerticeGrafo(g, criarVertice("v3", 10.0, 0.0, 2));
+    inserirVerticeGrafo(g, criarVertice("v4", 15.0, 0.0, 3));
+    inserirVerticeGrafo(g, criarVertice("v5", 20.0, 0.0, 4));
+    inserirArestaGrafo(g,"v1", "v2", "CEP390", "CEP391", 30.0, 10.0, "Rua Isabelle");
+    inserirArestaGrafo(g, "v3", "v4", "CEP419", "CEP420", 30.0, 10.0, "Rua Ana");
 
     ResultadoComponentes r = executarComponentes(g, 5.0);
 
     TEST_ASSERT_EQUAL_INT(3, getNumComponentes(r));
-    TEST_ASSERT_EQUAL_INT(5, getVerticesComponentes(r,0));
+    TEST_ASSERT_EQUAL_INT(5, totalVerticesEmComponentes(r));
+
+    int comp1 = componenteDoVertice(r, "v1");
+    int comp2 = componenteDoVertice(r, "v2");
+    int comp3 = componenteDoVertice(r, "v3");
+    int comp4 = componenteDoVertice(r, "v4");
+    int comp5 = componenteDoVertice(r, "v5");
+
+    TEST_ASSERT_EQUAL_INT(comp1, comp2);
+    TEST_ASSERT_EQUAL_INT(comp3, comp4);
+    TEST_ASSERT_NOT_EQUAL(comp1, comp3);
+    TEST_ASSERT_NOT_EQUAL(comp1, comp5);
+    TEST_ASSERT_NOT_EQUAL(comp3, comp5);
 
     liberarResultadoComponentes(r);
     liberarGrafo(g);
 }
 
 void teste_liberarResultadoComponentes_naoDeveCrashar(void) {
-    Grafo g = criarGrafo();
-    inserirVerticeGrafo(g,criarVertice("v1", 0.0, 0.0));
+    Grafo g = criarGrafo(1);
+    inserirVerticeGrafo(g,criarVertice("v1", 0.0, 0.0, 0));
     ResultadoComponentes r = executarComponentes(g, 0.0);
     liberarResultadoComponentes(r);
     liberarGrafo(g);
